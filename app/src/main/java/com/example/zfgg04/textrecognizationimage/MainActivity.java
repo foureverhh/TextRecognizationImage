e52dec3622a9e59dbd5e7c98351fad03153d50f7
foureverhh <foureverhh@gmail.com>
2018-10-29 12:33:28 +0100

    text in picture can be recognised

--- app/src/main/java/com/example/zfgg04/textrecognizationimage/MainActivity.java
+++ app/src/main/java/com/example/zfgg04/textrecognizationimage/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.zfgg04.textrecognizationimage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button btnProcess;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image_view);
+        btnProcess = findViewById(R.id.button_process);
+        textResult = findViewById(R.id.text_result);
+
+        final Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources()
+                ,R.drawable.hello);
+        imageView.setImageBitmap(bitmap);
+
+        btnProcess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext())
+                        .build();
+                if(!textRecognizer.isOperational()){
+                    Log.e("Error","Detector are not yet available");
+                }else{
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<TextBlock> items = textRecognizer.detect(frame);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for(int i = 0; i< items.size();i++){
+                        TextBlock item = items.valueAt(i);
+                        stringBuilder.append(item.getValue());
+                        stringBuilder.append("\n");
+                    }
+                    textResult.setText(stringBuilder.toString());
+                }
+            }
+        });
     }
 }
